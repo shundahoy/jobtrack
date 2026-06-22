d90d94aae66924cd3310ed56c6268e2b393875ee
shundahoy <shundahoy@gmail.com>
2026-06-22 17:06:13 +0900

    first commit

---
+++ src/main/java/com/jobtrack/JobtrackApplication.java
@@ -0,0 +1,12 @@
+package com.jobtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobtrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobtrackApplication.class, args);
+    }
+}
